3dee083e0b4f2589e29fd50759f310ed1a012fdb
mridul55 <mridul9650@gmail.com>
2023-01-08 16:12:25 +0600

    admin panel setup

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/test', function () {
-   // return view('welcome');
-   dd("hello");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function(){
+    return view('website.home');
+});
+
+Route::get('/about', function(){
+    return view('website.about');
+});
+
+Route::get('/category', function(){
+    return view('website.category');
+});
+Route::get('/contact', function(){
+    return view('website.contact');
+});
+Route::get('/post', function(){
+    return view('website.post');
+});
+//Admin Panel Route
+Route::get('/test',function(){
+   return view('admin.dashboard.index');
 });
